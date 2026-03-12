eece726cc23a523b1be8f1ff93b04f4a0de98698
Oliverbjarnvik <bjarnvik.oliver@gmail.com>
2026-03-12 15:32:23 +0100

    Uppdaterat attribut för klasserna "Book" & "Member"

--- src/Member.java
+++ src/Member.java
@@ -1,12 +1,12 @@
 
 public class Member {
 	public String name;
-	public String id; // Se nedan
+	public String id;
+	public int memberType; // Se nedan
 	/*
-	* 1. Undergraduate student
-	* 2. Postgraduate student
-	* 3. PhD student
-	* 4. Teacher
+	 * 1. Undergraduate student
+	 * 2. Postgraduate student
+	 * 3. PhD student
+	 * 4. Teacher
 	 */
-	public int memberType;
 }
