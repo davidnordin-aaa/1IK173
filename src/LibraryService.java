f2c438ded01aff8e37c8be43d8cdfbc6b72d4b84
Fiskett <oliver@fehre.com>
2026-03-13 10:21:12 +0100

    Första commit för att kolla ändringar

--- src/LibraryService.java
+++ src/LibraryService.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 
 public class LibraryService {
 
@@ -9,11 +11,29 @@ public class LibraryService {
 
 	public boolean borrow(String isbn, String memberId) {
 		boolean status = false;
-
+		Scanner scanner = new Scanner(System.in);
 		Member memberInfo = store.getMember(memberId);
 		Book book = store.getBook(isbn);
 
 		// more code here...
+		System.out.println("Is this the book you want to borrow?");
+		System.out.println(book.author + book.title + book.year + book.ISBN);
+		String input = scanner.nextLine();
+		if (Objects.equals(input, "Yes")){
+			System.out.println("Great we will try to get that sorted");
+		}
+		else{
+			System.out.println("We couldnt find that book, please double check your search.");
+			Main.main(null);
+		}
+		if (isBorrowed(book) == true){
+			System.out.println("That book was available and it is now borrowed on your account");
+		}
 		return status;
+	}
+	public boolean isBorrowed(Book book){
+		if(book.borrowed)
+
+		return true;
 	}
 }
